d1ecab8636db457c78ca97fea921b86434683121
David <davidharingri@gmail.com>
2021-08-13 14:52:10 +0200

    Changes on taildwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
-
 const tailwindColors = require('tailwindcss/colors');
 
 module.exports = {
-<<<<<<< HEAD
-  // important: 'body#body',
-  // prefix: 'tw-',
-=======
-  important: 'body',
->>>>>>> 22f173cbaa61a90b3cb230a7e29075ba47204d02
-  purge: ['./src/**/*.{html,vue,js,ts,jsx,tsx}', './public/**/*.{html,vue,js,ts,jsx,tsx}'],
+  // important: 'body', 
+  // prefix: 'tw-',  purge: ['./src/**/*.{html,vue,js,ts,jsx,tsx}', './public/**/*.{html,vue,js,ts,jsx,tsx}'],
+
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: tailwindColors,
