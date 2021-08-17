04190977f97169e0424bc25a6cd32463238db0c2
David <davidharingri@gmail.com>
2021-08-17 11:40:40 +0200

    Added form for Adding new Style.

--- src/pages/Elements/StyleForm.vue
+++ src/pages/Elements/StyleForm.vue
@@ -12,6 +12,154 @@
         </div>
 
         <button class="bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="hideModal()">Choose Style</button>
+
+        <div class="border-b-2 mt-6"></div>
+
+        <div class="px-6 text-left text-2xl capitalize font-medium text-gray-500 uppercase tracking-wider mt-4">Add New Style</div>
+
+        <div class="mt-8 mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Image Height
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 200px"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Title Font Family
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. Font Sans"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Title Display
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. none"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Title Font Weight
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 600"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Title Font Size
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 18px"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Subtitle Font Color
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. #E7272D"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Subtitle Font Weight
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 600"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Subtitle Font Size
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 14px"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Description Font Color
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. #000000"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Description Font Weight
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 300"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Description Font Size
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 14px"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Price Font Weight
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 300"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Price Font Size
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 18px"
+            />
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                Price Width
+            </label>
+            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="input"
+            placeholder="e.g. 70px"
+            />
+        </div>
+
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="hideModal()">Add Style</button>
+
     </div>
 </template>
 
