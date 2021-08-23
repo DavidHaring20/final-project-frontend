3fab5211ead0944149200811e5f7bfbe94fa9720
David <davidharingri@gmail.com>
2021-08-23 09:26:44 +0200

    Added Update Style.

--- src/pages/Elements/StyleForm.vue
+++ src/pages/Elements/StyleForm.vue
@@ -16,7 +16,7 @@
                     <div :style="{fontWeight: style.item_price_font_weight, fontSize: style.item_price_font_size, width: style.item_price_width}">Price</div>
                 </div>
                 <div class="flex justify-center items-center w-1/6">
-                    <Button btnText="Edit"/>
+                    <Button @clicked="editStyle(style.id, style)" btnText="Edit"/>
                 </div>
             </div> 
         </div>
@@ -25,150 +25,166 @@
 
         <div class="border-b-2 mt-6"></div>
 
-        <div class="px-6 text-left text-2xl capitalize font-medium text-gray-500 uppercase tracking-wider mt-4">Add New Style</div>
+        <div v-if="modalTitle == 'editStyle'">
+            <div class="px-6 text-left text-2xl capitalize font-medium text-gray-500 uppercase tracking-wider mt-4">Edit Style</div>
 
-        <div class="mt-8 mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Image Height
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 200px"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Title Font Family
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. Font Sans"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Title Display
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. none"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Title Font Weight
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 600"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Title Font Size
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 18px"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Subtitle Font Color
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. #E7272D"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Subtitle Font Weight
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 600"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Subtitle Font Size
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 14px"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Description Font Color
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. #000000"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Description Font Weight
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 300"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Description Font Size
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 14px"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Price Font Weight
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 300"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Price Font Size
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 18px"
-            />
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
-                Price Width
-            </label>
-            <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            name="input"
-            placeholder="e.g. 70px"
-            />
-        </div>
+            <div class="mt-8 mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Image Height
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.header_image_max_height"
+                placeholder="e.g. 200px"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Title Font Family
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_title_font_family"
+                placeholder="e.g. Font Sans"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Title Display
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_title_display"
+                placeholder="e.g. none"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Title Font Weight
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_title_font_weight"
+                placeholder="e.g. 600"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Title Font Size
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_title_font_size"
+                placeholder="e.g. 18px"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Subtitle Font Color
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_subtitle_color"
+                placeholder="e.g. #E7272D"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Subtitle Font Weight
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_subtitle_font_weight"
+                placeholder="e.g. 600"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Subtitle Font Size
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_subtitle_font_size"
+                placeholder="e.g. 14px"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Description Font Color
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_description_color"
+                placeholder="e.g. #000000"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Description Font Weight
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_description_font_weight"
+                placeholder="e.g. 300"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Description Font Size
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_description_font_size"
+                placeholder="e.g. 14px"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Price Font Weight
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_price_font_weight"
+                placeholder="e.g. 300"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Price Font Size
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_price_font_size"
+                placeholder="e.g. 18px"
+                />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-xs mb-2 uppercase" for="input">
+                    Price Width
+                </label>
+                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="input"
+                v-model="editedStyle.item_price_width"
+                placeholder="e.g. 70px"
+                />
+            </div>
 
-        <button class="bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="hideModal()">Add Style</button>
+            <button class="bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="confirmEditedStyle()">Save Changes</button>
+        </div>
 
     </div>
 </template>
@@ -186,7 +202,9 @@ export default {
     data() { 
         return{
             styles: undefined,
-            styleID: undefined
+            styleID: undefined,
+            modalTitle: String,
+            editedStyle: Object
         }
     },
     
@@ -195,6 +213,41 @@ export default {
     },
 
     methods: {
+        editStyle(id, style) {
+            this.modalTitle = 'editStyle';
+            this.editedStyle = style;
+        },
+
+        confirmEditedStyle() {
+            // console.log(this.editedStyle);
+
+            this.$service.API.patch('/styles/' + this.editedStyle.id, {
+                headerImageMaxHeight: this.editedStyle.header_image_max_height,
+                itemTitleFontFamily: this.editedStyle.item_title_font_family,
+                itemTitleDisplay: this.editedStyle.item_title_display,
+                itemSubtitleColor: this.editedStyle.item_subtitle_color,
+                itemDescriptionColor: this.editedStyle.item_description_color,
+                itemTitleFontWeight: this.editedStyle.item_title_font_weight,
+                itemSubtitleFontWeight: this.editedStyle.item_subtitle_font_weight,
+                itemDescriptionFontWeight: this.editedStyle.item_description_font_weight,
+                itemPriceFontWeight: this.editedStyle.item_price_font_weight,
+                itemTitleFontSize: this.editedStyle.item_title_font_size,
+                itemSubtitleFontSize: this.editedStyle.item_subtitle_font_size,
+                itemDescriptionFontSize: this.editedStyle.item_description_font_size,
+                itemPriceFontSize: this.editedStyle.item_price_font_size,
+                itemPriceWidth: this.editedStyle.item_price_width
+            })
+            .then(response => response.data)
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+            this.modalTitle = '';
+        },
+
         confirmSelectedStyle() {
             let self = this;
 
