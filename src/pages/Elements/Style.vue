c169e6c5c208a5eebf8a8abea4f1b4b7c5fb47fa
David <davidharingri@gmail.com>
2021-08-31 09:47:10 +0200

    Display style in masterStyle, create new and delete.

--- src/pages/Elements/Style.vue
+++ src/pages/Elements/Style.vue
@@ -1,20 +1,10 @@
 <template>
     <tr class="border-b-2">
         <td class="text-center p-2">{{ index + 1 }}</td>
-        <td class="text-center p-2">{{ styleObject.header_image_max_height }}</td>
-        <td class="text-center p-2">{{ styleObject.item_title_font_family }}</td>
-        <td class="text-center p-2">{{ styleObject.item_title_display }}</td>
-        <td class="text-center p-2">{{ styleObject.item_subtitle_color }}</td>
-        <td class="text-center p-2">{{ styleObject.item_description_color }}</td>
-        <td class="text-center p-2">{{ styleObject.item_title_font_weight }}</td>
-        <td class="text-center p-2">{{ styleObject.item_subtitle_font_weight }}</td>
-        <td class="text-center p-2">{{ styleObject.item_description_font_weight }}</td>
-        <td class="text-center p-2">{{ styleObject.item_price_font_weight }}</td>
-        <td class="text-center p-2">{{ styleObject.item_title_font_size }}</td>
-        <td class="text-center p-2">{{ styleObject.item_subtitle_font_size }}</td>
-        <td class="text-center p-2">{{ styleObject.item_description_font_size }}</td>
-        <td class="text-center p-2">{{ styleObject.item_price_font_size }}</td>
-        <td class="text-center p-2">{{ styleObject.item_price_width }}</td>
+
+        <td class="text-center p-2">{{ styleObject.key }}</td>
+        <td class="text-center p-2">{{ styleObject.value }}</td>
+        
         <td class="text-center p-2" @click="emitDelete(styleObject.id)"><Button btnText="Delete" class="text-center"/></td>
     </tr>
 </template>
--- src/pages/StyleAdministration.vue
+++ src/pages/StyleAdministration.vue
@@ -7,20 +7,8 @@
             <thead>
                 <tr class="border-4">
                     <th class="ml-4 p-4">Number</th>
-                    <th class="ml-4 p-4">header_image_max_height</th>
-                    <th class="ml-4 p-4">item_title_font_family</th>
-                    <th class="ml-4 p-4">item_title_display</th>
-                    <th class="ml-4 p-4">item_subtitle_color</th>
-                    <th class="ml-4 p-4">item_description_color</th>
-                    <th class="ml-4 p-4">item_title_font_weight</th>
-                    <th class="ml-4 p-4">item_subtitle_font_weight</th>
-                    <th class="ml-4 p-4">item_description_font_weight</th>
-                    <th class="ml-4 p-4">item_price_font_weight</th>
-                    <th class="ml-4 p-4">item_title_font_size</th>
-                    <th class="ml-4 p-4">item_subtitle_font_size</th>
-                    <th class="ml-4 p-4">item_description_font_size</th>
-                    <th class="ml-4 p-4">item_price_font_size</th>
-                    <th class="ml-4 p-4">item_price_width</th>
+                    <th class="ml-4 p-4">Key</th>
+                    <th class="ml-4 p-4">Value</th>
                     <th class="ml-4 p-4">Delete</th>
                 </tr>
             </thead>
@@ -31,169 +19,43 @@
                     @delete-style="deleteStyle($event.deletedStyleId)"></Style>
             </tbody>
         </table>
-        <div>
-            <button class="ml-16 bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="openCreateNewStyleDialog()">Create New Style</button>
+
+        <div v-if="emitMessage == ''">
+            <div class="h-72">
+                <button class="ml-16 bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="openCreateNewStyleDialog()">Create New Style</button>
+            </div>
         </div>
 
         <div v-if="emitMessage == 'Create New Style'" class="ml-16 mt-16">
             <hr class="w-2/5 mt-4 mb-2 ml-6">
-            <h2 class="mt-6 mb-8 px-6 text-left text-2xl capitalize font-medium text-gray-500 uppercase tracking-wider mt-4">New Style</h2>
+            <h2 class="mt-6 mb-8 px-6 text-left text-2xl capitalize font-medium text-gray-500 uppercase tracking-wider mt-4">New Style Property</h2>
 
             <div class="mt-8 mb-4">
                 <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    headerImageMaxHeight
+                    Key
                 </label>
                 <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             
                 name="input"
-                v-model="header_image_max_height"
-                placeholder="e.g. 200px"
+                v-model="key"
+                placeholder="e.g. itemPriceWidth"
                 />
             </div>
-            <div class="mb-4">
+            <div class="mt-8 mb-4">
                 <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemTitleFontFamily
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                name="input"
-                v-model="item_title_font_family"
-                placeholder="e.g. Font Sans"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemTitleDisplay
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                name="input"
-                v-model="item_title_display"
-                placeholder="e.g. none"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemTitleFontWeight
+                    Value
                 </label>
                 <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             
                 name="input"
-                v-model="item_title_font_weight"
-                placeholder="e.g. 600"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemTitleFontSize
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_title_font_size"
-                placeholder="e.g. 18px"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemSubtitleColor
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_subtitle_color"
-                placeholder="e.g. #E7272D"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemSubtitleFontWeight
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_subtitle_font_weight"
-                placeholder="e.g. 600"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemSubtitleFontSize
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_subtitle_font_size"
-                placeholder="e.g. 14px"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemDescriptionColor
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_description_color"
-                placeholder="e.g. #000000"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemDescriptionFontWeight
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_description_font_weight"
-                placeholder="e.g. 300"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemDescriptionFontSize
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_description_font_size"
-                placeholder="e.g. 14px"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemPriceFontWeight
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_price_font_weight"
-                placeholder="e.g. 300"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemPriceFontSize
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_price_font_size"
-                placeholder="e.g. 18px"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-xs mb-2 " for="input">
-                    itemPriceWidth
-                </label>
-                <input class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            
-                name="input"
-                v-model="item_price_width"
-                placeholder="e.g. 70px"
+                v-model="value"
+                placeholder="e.g. 200px, #000000"
                 />
             </div>
 
-
-            <button class="ml-16 mt-6 bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="saveNewStyle()">Save Changes</button>
+            <div class="h-72">
+                <button class="ml-16 mt-6 bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="saveNewStyle()">Save Changes</button>
+            </div>
         </div>
     </div>
 </template>
@@ -221,27 +83,15 @@ export default {
         return {
             styles: Array,
             newStyle: Object,
-            emitMessage: String,
-            header_image_max_height: undefined, 
-            item_title_font_family: undefined, 
-            item_title_display: undefined, 
-            item_title_font_weight: undefined, 
-            item_title_font_size: undefined, 
-            item_subtitle_color: undefined, 
-            item_subtitle_font_weight: undefined, 
-            item_subtitle_font_size: undefined, 
-            item_description_color: undefined, 
-            item_description_font_weight: undefined, 
-            item_description_font_size: undefined,
-            item_price_font_weight: undefined, 
-            item_price_font_size: undefined, 
-            item_price_width: undefined
+            emitMessage: "",
+            key: "",
+            value: ""
         }
     },
 
     methods: {
         deleteStyle(id) {
-            this.$service.API.delete('/styles/' + id)
+            this.$service.API.delete('/styleMaster/' + id)
             .then(response => response.data)
             .then(data => {
                 console.log(data);
@@ -258,30 +108,16 @@ export default {
         },
 
         saveNewStyle() {
-            this.$service.API.post('/styles', {
-                'headerImageMaxHeight': this.header_image_max_height,
-                'itemTitleFontFamily': this.item_title_font_family,
-                'itemTitleDisplay': this.item_title_display,
-                'itemSubtitleColor': this.item_subtitle_color, 
-                'itemDescriptionColor': this.item_description_color,
-                'itemTitleFontWeight': this.item_title_font_weight,
-                'itemSubtitleFontWeight': this.item_subtitle_font_weight,
-                'itemDescriptionFontWeight': this.item_description_font_weight,
-                'itemPriceFontWeight': this.item_price_font_weight,
-                'itemTitleFontSize': this.item_title_font_size,
-                'itemSubtitleFontSize': this.item_subtitle_font_size,
-                'itemDescriptionFontSize': this.item_description_font_size,
-                'itemPriceFontSize': this.item_price_font_size,
-                'itemPriceWidth': this.item_price_width
+            this.$service.API.post('/styleMaster', {
+                'key': this.key,
+                'value': this.value
             })
             .then(response => response.data)
             .then(data => {
-                // console.log(data);
-                // console.log(data.data);
-                // console.log(data.data.newStyle);
+                console.log(data);
 
                 // ADD TO STYLES ARRAY SO IT APPEARS IN DOM
-                this.styles.push(data.data.newStyle);
+                this.styles.push(data.newStyleMasterProperty);
             });
 
             this.closeCreateNewStyleDialog();
@@ -296,11 +132,11 @@ export default {
         },
 
         getStyles() {
-            this.$service.API.get('/styles')
+            this.$service.API.get('/styleMaster')
             .then(response => response.data)
             .then(data => {
-                console.log(data.data);
-                this.styles = data.data.styles;
+                console.log(data);
+                this.styles = data.styleMaster;
             })
             .catch(error => {
                 console.log(error);
