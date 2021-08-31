bb58215a687bc28d811ef186d9697ef9265b456a
David <davidharingri@gmail.com>
2021-08-31 09:16:46 +0200

    Added loading

--- src/pages/Elements/StyleForm.vue
+++ src/pages/Elements/StyleForm.vue
@@ -17,25 +17,25 @@
             </div>           
         </div>
 
-        <button class="ml-16 mt-6 bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="saveChange(restaurantId)">Save Changes</button>
+        <div v-if="loadingMode == 'on'">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-notch" 
+            class="svg-inline--fa fa-circle-notch fa-w-16 w-1/12 mt-8 ml-48 animate-spin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="rgb(55, 65, 81)" d="M288 39.056v16.659c0 10.804 7.281 20.159 17.686 23.066C383.204 100.434 440 171.518 440 256c0 101.689-82.295 184-184 184-101.689 0-184-82.295-184-184 0-84.47 56.786-155.564 134.312-177.219C216.719 75.874 224 66.517 224 55.712V39.064c0-15.709-14.834-27.153-30.046-23.234C86.603 43.482 7.394 141.206 8.003 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.788 504 256c0-115.633-79.14-212.779-186.211-240.236C302.678 11.889 288 23.456 288 39.056z"></path></svg>
+        </div>
+
+        <button id="save-change-button" class="ml-16 mt-6 bg-green-500 hover:bg-green-700 text-white font-bold text-xs py-2 px-6 rounded transition-colors duration-300" @click="saveChange(restaurantId)">Save Changes</button>
     </div>
 </template>
 
 <script>
-import Button from './Button.vue';
-
 export default {
     name: 'StyleForm',
-
-    components: {
-        Button
-    },
 
     data() { 
         return{
             styles: undefined,
             originalStyleValues: undefined,
-            editedStyle: Object
+            editedStyle: Object,
+            loadingMode: 'off'
         }
     },
 
@@ -49,6 +49,12 @@ export default {
 
     methods: {
         saveChange(id) {
+            console.log('click');
+            this.loadingMode = 'on';
+
+            let saveChangeButton = document.getElementById('save-change-button');
+            saveChangeButton.setAttribute('class', 'hidden');
+
             // This is a method for updating a style. Since we have style property that consist of key
             // and value, we will make array for one and for two and call patch HTTP method multiple time, but only 
             // for values that have changed from original value.
@@ -93,6 +99,11 @@ export default {
                     console.log(error);
                 });
             }
+            
+            setTimeout(() => {
+                this.loadingMode = 'off';
+                this.hideModal();
+            }, 5000);
         },
 
         hideModal() {
