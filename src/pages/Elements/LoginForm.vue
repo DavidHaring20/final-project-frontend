ca8111fb7951e0230baed49c06dcb72c5d20cecc
David <davidharingri@gmail.com>
2021-11-26 12:33:40 +0100

    Placed old Login Form in new layout.

--- src/pages/Elements/LoginForm.vue
+++ src/pages/Elements/LoginForm.vue
@@ -3,60 +3,67 @@
 
         <!-- Left side - Login form -->
         <div class="w-full">
+            <!-- Transparent Layer -->
+            <div class="ml-24 w-4/12 absolute" style="height: 700px;">
+                <div class="mt-36 flex flex-col bg-white p bg-opacity-70">
+
+                    <!-- Login -->
+                    <p class="text-2xl text-center font-bold mt-12">PocketMenu Login</p>
+
+                    <!-- Login Form -->
+                    <div>
+                        <!-- <div class="flex flex-col mt-24 justify-content content-center" style=""> -->
+                        <div>
+                            <p v-if="message" v-bind:class="messageStyle" style="max-width: 35ch; max-height: 5rem; justify-items:center; align-self-center"> {{ message }}</p>
+                        </div>
+                            <div class="mb-8">
+                                <div class="flex flex-row mb-5">
+                                    <div>
+                                        <p class="pr-5 font-bold">E-mail: </p>
+                                    </div>
+                                    <div>
+                                        <input
+                                            @keyup.enter="requestVerificationCode()"
+                                            v-model="email"
+                                            type="text" 
+                                            name="e-mail"
+                                            class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                        >
+                                    </div>
+                                </div>
+                                
+                                <div v-if="renderPasscodeSection">
+                                    <button 
+                                        @click="requestVerificationCode()"
+                                        class="bg-gray-200 ml-3 w-full px-3 py-2 rounded-md text-gray-500 hover:bg-gray-300 hover:text-white transition-colors duration-500"
+                                    >Send Verification Code</button>
+                                </div>
+                            </div>
+                            
+                            <div v-if="renderPasscodeSection == false">
+                                <div class="flex flex-row mb-5">
+                                        <p class="pr-5 font-bold">Passcode: </p>
+                                        <input 
+                                            @keyup.enter="authenticate()"
+                                            v-model="passcode"
+                                            type="text"
+                                            name="passcode" 
+                                            class="shadow appearance-none border rounded ml-4 w-59 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                        >
+                                </div> 
+
+                                <button 
+                                    @click="authenticate()"
+                                    class="bg-gray-200 ml-3 w-full px-3 py-2 rounded-md text-gray-500 hover:bg-gray-300 hover:text-white transition-colors duration-500"
+                                >Log In</button>
+                            </div>
+                        </div>
+                    <!-- </div> -->
+                </div>
+            </div>
+            
             <!-- Image -->
             <img :src="imgLeft" alt="Menu picture." class="w-full" style="height: 700px;">
-
-            <!-- Transparent Layer -->
-
-            <!-- Login Form -->
-            <!-- <div class="z-10 h-screen relative flex flex-col justify-center items-center z-10">
-                <div class="border-solid bg-white p-20 text-center border-gray-600 shadow-xl" style="border-width: 4px; border-radius: 10%">
-                <div class="h-32 mb-8 justify-center flex">
-                    <p v-if="message" v-bind:class="messageStyle" style="max-width: 35ch; max-height: 5rem; justify-items:center; align-self-center"> {{ message }}</p>
-                </div>
-                    <div class="mb-8">
-                        <div class="flex flex-row mb-5">
-                            <div>
-                                <p class="pr-5 font-bold">E-mail: </p>
-                            </div>
-                            <div>
-                                <input
-                                    @keyup.enter="requestVerificationCode()"
-                                    v-model="email"
-                                    type="text" 
-                                    name="e-mail"
-                                    class="shadow appearance-none border rounded w-60 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                >
-                            </div>
-                        </div>
-                        
-                        <div v-if="renderPasscodeSection">
-                            <button 
-                                @click="requestVerificationCode()"
-                                class="bg-gray-200 ml-3 w-full px-3 py-2 rounded-md text-gray-500 hover:bg-gray-300 hover:text-white transition-colors duration-500"
-                            >Send Verification Code</button>
-                        </div>
-                    </div>
-                    
-                    <div v-if="renderPasscodeSection == false">
-                        <div class="flex flex-row mb-5">
-                                <p class="pr-5 font-bold">Passcode: </p>
-                                <input 
-                                    @keyup.enter="authenticate()"
-                                    v-model="passcode"
-                                    type="text"
-                                    name="passcode" 
-                                    class="shadow appearance-none border rounded ml-4 w-59 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                >
-                        </div> 
-
-                        <button 
-                            @click="authenticate()"
-                            class="bg-gray-200 ml-3 w-full px-3 py-2 rounded-md text-gray-500 hover:bg-gray-300 hover:text-white transition-colors duration-500"
-                        >Log In</button>
-                    </div>
-                </div>
-            </div> -->
 
             <!-- Bottom text -->
             <div class="w-full">
