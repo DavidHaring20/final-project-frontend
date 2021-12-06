05f7d5dba8e9e8cc0d9563ff4ba3584f04cc29ae
David <davidharingri@gmail.com>
2021-12-06 12:02:56 +0100

    Added new look to the navbar.

--- src/pages/Elements/Navbar.vue
+++ src/pages/Elements/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="header-2 bg-gray-100 shadow rounded-b-lg">
+  <div class="header-2 bg-black h-10">
       <nav class="py-1 md:py-1">
         <div class="container px-4 mx-auto md:flex md:items-center">
           <div class="flex justify-between">
             <button @click="goToHome()" class="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="gray">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
               </svg>
-              <div class="px-2 font-bold text-gray-500 font-mono">
+              <div class="px-4 font-bold text-xl text-white font-mono">
                 PocketMenu
               </div>
             </button>
@@ -17,15 +17,22 @@
           </div>
 
 
-          <div class="flex flex-row-reverse w-screen">
-            <a href="/" class="p-1 text-xs lg:px-2 md:mx-2 text-gray-600 text-center border border-transparent rounded hover:bg-gray-300 hover:text-white transition-colors duration-300">Log Out</a>
+          <div class="flex  w-screen justify-self-end">
+            <div class="w-full flex justify-center ml-72">
+              <div v-if="userRole == 'admin'" style="border-color: black white white black; transform: rotate(45deg)" class="w-5 h-5 border-4"></div>
+            </div>
+
+            <div class="w-full flex justify-end">
+              <a href="/" class="px-8 text-sm text-white text-center border border-transparent rounded hover:bg-white hover:text-black transition-colors duration-300">Log Out</a>
+            </div>
             
-            <div v-if="userRole == 'admin'">
+          </div>
+
+          <div v-if="clicked == true">
               <button @click="goToLanguageAdministration()" class="p-1 text-xs lg:px-2 md:mx-2 text-gray-600 text-center border border-transparent rounded hover:bg-gray-300 hover:text-white transition-colors duration-300">Language Administration</button>
               <button @click="goToStyleAdministration()" class="p-1 text-xs lg:px-2 md:mx-2 text-gray-600 text-center border border-transparent rounded hover:bg-gray-300 hover:text-white transition-colors duration-300">Style Administration</button>
               <button @click="goToSlugOrIdAdministration()" class="p-1 text-xs lg:px-2 md:mx-2 text-gray-600 text-center border border-transparent rounded hover:bg-gray-300 hover:text-white transition-colors duration-300">Slug/ID Administration</button>
             </div>
-          </div>
         </div>
       </nav>
     </div>
@@ -34,7 +41,6 @@
 <script>
 export default {
   name: 'Navbar',
-
 
   data () {
     return {
